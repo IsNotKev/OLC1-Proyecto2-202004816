c3bdf68de74d21cc2d1b8570bf8a4ac70f997e39
Kevin <kevinlemus2002@gmail.com>
2022-04-10 23:52:51 -0600

    Conectando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (    
     <div className="App">
-      <h1>CODIGO KEVIN</h1>
       <textarea id="w3review" name="w3review" rows="4" cols="50">Hola Mundo</textarea>
     </div>  
   );
