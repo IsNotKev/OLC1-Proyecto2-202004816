5abb1c0476ed95497bb87e9a09037eb4fa7e2286
Kevin <kevinlemus2002@gmail.com>
2022-04-13 18:42:45 -0600

    Ahi vamos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,65 @@
-import logo from './logo.svg';
+import Editor from '@monaco-editor/react';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
-  return (    
-    <div className="App">
-      <textarea id="w3review" name="w3review" rows="4" cols="50">Hola Mundo</textarea>
-    </div>  
+
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor; 
+  }
+  
+
+  function ejecutar() {
+    var obj = {'codigo':editorRef.current.getValue()}
+    
+    fetch(`http://localhost:5000/ejecutar`, {
+      method: 'POST',
+      body:JSON.stringify(obj),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }     
+    })
+      .then(res => res.json())
+      .catch(err => {
+        console.error('Error:', err)
+        alert("Ocurrio un error, ver la consola")
+      })
+      .then(response => {
+        console.log(response.message)  ; 
+        document.getElementById('consola').textContent = response.message;
+        //editorRef.current.getModel().setValue(response.message);
+      })
+  }
+
+  return (
+    
+    <div className='min-h-screen min-w-full bg-gray-500 flex justify-center items-center flex-col gap-10'>
+      <button onClick={ejecutar} type="button" class="btn btn-outline-dark" style={{align:'center',marginTop:'2%'}}>Ejecutar</button>
+      <p>
+
+      </p>
+      <div className='max-w-[60rem] w-full flex flex-col gap-5'>      
+        <div className='w-full rounded-xl'>
+          <Editor
+            height="70vh"
+            defaultLanguage="typescript"
+            defaultValue="// CompScript :)"
+            onMount={handleEditorDidMount}
+            className={'rounded-xl'}
+            theme="vs-dark"
+          />
+        </div>    
+      </div>
+      <p>
+
+      </p>
+      <textarea disabled style={{width:'100%',height:'100px',background:'gray',color:'black'}} id="consola">
+      
+      </textarea>
+    </div>
   );
 }
 
--- my-backend/src/index.js
+++ my-backend/src/index.js
@@ -3,19 +3,27 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello World'
+        message: 'Hello World 2.0'
     });
 });
 
-app.get('/:name', (req, res) => {
+/*app.get('/:name', (req, res) => {
     let name = req.params.name;
 
     res.json({
         message: `Hello ${name}`
+    });
+});*/
+
+app.post('/ejecutar', function (req, res){
+    //let cod = req.body.codigo;
+    res.json({
+        message: `El código es: ${req.body.codigo}`
     });
 });
 
